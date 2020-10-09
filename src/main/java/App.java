32cbf10433138770b6106cd43606158a60ac42fb
Anna <mghnsm@gmail.com>
2020-10-09 21:50:42 +0300

    Scripted auto-commit on change (2020-10-09 21:50:42) by gitwatch.sh

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,18 @@
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
-            System.err.println("Usage: App <input path> <output path>");
+            System.err.println("Usage: App ");
             System.exit(-1);
         }
         Job job = Job.getInstance();
         job.setJarByClass(WordCountApp.class);
-        job.setJobName("");
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         job.setNumReduceTasks(2);
