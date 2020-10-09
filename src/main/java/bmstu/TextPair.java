1b373ce59bb0da521fc653ebbb498f38478f3091
Anna <mghnsm@gmail.com>
2020-10-09 23:49:11 +0300

    Scripted auto-commit on change (2020-10-09 23:49:11) by gitwatch.sh

--- src/main/java/bmstu/TextPair.java
+++ src/main/java/bmstu/TextPair.java
@@ -3,6 +3,10 @@ package bmstu;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class TextPair implements WritableComparable<TextPair> {
     private Text key;
     private Text value;
@@ -10,9 +14,15 @@ public class TextPair implements WritableComparable<TextPair> {
     public Text getKey() { return key; }
     public Text getValue() { return value; }
 
-    public void write() {}
+    public void write(DataOutput out) {
+        key.write(out);
+        value.write(out);
+    }
 
-    public void readFields() {}
+    public void readFields(DataInput in) throws IOException {
+        key.readFields(in);
+        value.readFields(in);
+    }
 
     public int compareTo() {}
 
