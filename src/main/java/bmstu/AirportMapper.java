eda8d4dd676b2f756a021392e747a5de2235295a
Anna <mghnsm@gmail.com>
2020-10-09 23:18:26 +0300

    Scripted auto-commit on change (2020-10-09 23:18:26) by gitwatch.sh

--- src/main/java/bmstu/AirportMapper.java
+++ src/main/java/bmstu/AirportMapper.java
@@ -1,4 +1,12 @@
 package bmstu;
 
 public class AirportMapper extends Mapper {
+    @Override
+    public void map(Text key, TupleWritable value,
+                    OutputCollector<Text, Text> output,
+                    Reporter reporter) throws IOException {
+        Text call = (Text) value.get(0);
+        Text system = (Text) value.get(1);
+        output.collect(call, system);
+    }
 }
--- src/main/java/bmstu/DelayMapper.java
+++ src/main/java/bmstu/DelayMapper.java
@@ -3,4 +3,12 @@ package bmstu;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class DelayMapper extends Mapper {
+    @Override
+    public void map(Text key, TupleWritable value,
+                    OutputCollector<Text, Text> output,
+                    Reporter reporter) throws IOException {
+        Text call = (Text) value.get(0);
+        Text system = (Text) value.get(1);
+        output.collect(call, system);
+    }
 }
