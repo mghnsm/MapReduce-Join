0dbbfc8e84a0ba3f29a7e7a3f9e3a98fae6617a9
Anna <mghnsm@gmail.com>
2020-10-09 21:49:42 +0300

    Scripted auto-commit on change (2020-10-09 21:49:42) by gitwatch.sh

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,15 @@
 public class App {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: App <input path> <output path>");
+            System.exit(-1);
+        }
+        Job job = Job.getInstance();
+        job.setJarByClass(WordCountApp.class);
+        job.setJobName("");
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
