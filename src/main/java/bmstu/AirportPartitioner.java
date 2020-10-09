8700ce7f533352a134198299ed5c78674ce8b848
Anna <mghnsm@gmail.com>
2020-10-09 23:02:51 +0300

    Scripted auto-commit on change (2020-10-09 23:02:51) by gitwatch.sh

--- src/main/java/bmstu/AirportPartitioner.java
+++ src/main/java/bmstu/AirportPartitioner.java
@@ -3,7 +3,7 @@ package bmstu;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class AirportPartitioner extends Partitioner<K, V> {
-    public int getPartition(K, V, numPartitions) {
+    public int getPartition(key, value, numPartitions) {
         return (key.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
     }
 }
