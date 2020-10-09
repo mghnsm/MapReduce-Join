e3b1da4d8d218d30675dc9761e748b7668402d68
Anna <mghnsm@gmail.com>
2020-10-09 23:42:10 +0300

    Scripted auto-commit on change (2020-10-09 23:42:10) by gitwatch.sh

--- src/main/java/bmstu/TextPair.java
+++ src/main/java/bmstu/TextPair.java
@@ -1,7 +1,11 @@
 package bmstu;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
 
 public class TextPair implements WritableComparable<TextPair> {
+    private Text key;
+    private Text value;
 
+    
 }
