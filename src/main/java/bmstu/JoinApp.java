0e0e45b36c64aa4b0c7c3a7cfcef53f58e97506c
Anna <mghnsm@gmail.com>
2020-10-09 21:53:15 +0300

    Scripted auto-commit on change (2020-10-09 21:53:15) by gitwatch.sh

--- src/main/java/bmstu/JoinApp.java
+++ src/main/java/bmstu/JoinApp.java
@@ -1,3 +1,5 @@
+package bmstu;
+
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.fs.Path;
@@ -5,10 +7,10 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 
-public class App {
+public class JoinApp {
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
-            System.err.println("Usage: App ");
+            System.err.println("Usage: JoinApp ...");
             System.exit(-1);
         }
     }
