22df3c6b42df186e4f64bda5c3c59f34072efc8b
Anna <mghnsm@gmail.com>
2020-10-09 23:10:18 +0300

    Scripted auto-commit on change (2020-10-09 23:10:18) by gitwatch.sh

--- src/main/java/bmstu/AirportComparator.java
+++ src/main/java/bmstu/AirportComparator.java
@@ -1,8 +1,10 @@
 package bmstu;
 
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportComparator extends WritableComparator {
+
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
         return a.compareTo(b);
--- src/main/java/bmstu/AirportPartitioner.java
+++ src/main/java/bmstu/AirportPartitioner.java
@@ -3,6 +3,8 @@ package bmstu;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class AirportPartitioner extends Partitioner<K, V> {
+
+    @Override
     public int getPartition(key, value, numReduceTasks) {
         return (key.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
     }
