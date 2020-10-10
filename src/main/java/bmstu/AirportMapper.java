5cf13cde6b4c429ff430c4aaaf739c915428fb0a
Anna <mghnsm@gmail.com>
2020-10-10 11:10:14 +0300

    Scripted auto-commit on change (2020-10-10 11:10:14) by gitwatch.sh

--- src/main/java/bmstu/AirportMapper.java
+++ src/main/java/bmstu/AirportMapper.java
@@ -12,6 +12,8 @@ import java.io.IOException;
 public class AirportMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException {
+
+
         TextPair code;
         Text name;
 
