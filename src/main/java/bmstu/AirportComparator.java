7ccf445e9bee5634e7cea2d70e77117e4e050fb6
Anna <mghnsm@gmail.com>
2020-10-10 10:59:46 +0300

    Scripted auto-commit on change (2020-10-10 10:59:46) by gitwatch.sh

--- src/main/java/bmstu/AirportComparator.java
+++ src/main/java/bmstu/AirportComparator.java
@@ -6,6 +6,8 @@ import org.apache.hadoop.io.WritableComparator;
 public class AirportComparator extends WritableComparator {
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
-        return a.compareTo(b);
+        TextPair a1 = (TextPair) a;
+        TextPair b1 = (TextPair) b;
+        return a1.getKey().compareTo(b1.getKey());
     }
 }
