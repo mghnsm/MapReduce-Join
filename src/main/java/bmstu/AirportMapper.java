ad036a4ac4ef5e2cdc0b213d11ad808334ac1685
Anna <mghnsm@gmail.com>
2020-10-10 10:36:37 +0300

    Scripted auto-commit on change (2020-10-10 10:36:37) by gitwatch.sh

--- src/main/java/bmstu/AirportMapper.java
+++ src/main/java/bmstu/AirportMapper.java
@@ -12,6 +12,6 @@ import java.io.IOException;
 public class AirportMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException {
-        
+
     }
 }
--- src/main/java/bmstu/DelayMapper.java
+++ src/main/java/bmstu/DelayMapper.java
@@ -1,5 +1,6 @@
 package bmstu;
 
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
@@ -8,13 +9,9 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class DelayMapper extends Mapper {
+public class DelayMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
-    public void map(Text key, TupleWritable value,
-                    OutputCollector<Text, Text> output,
-                    Reporter reporter) throws IOException {
-        Text call = (Text) value.get(0);
-        Text system = (Text) value.get(1);
-        output.collect(call, system);
+    public void map(LongWritable key, Text value, Context context) throws IOException {
+
     }
 }
