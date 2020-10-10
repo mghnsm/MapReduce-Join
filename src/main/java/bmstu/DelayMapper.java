33daa2451fd1fb4be9fa3107ff680dcefd174f20
Anna <mghnsm@gmail.com>
2020-10-10 10:57:31 +0300

    Scripted auto-commit on change (2020-10-10 10:57:31) by gitwatch.sh

--- src/main/java/bmstu/DelayMapper.java
+++ src/main/java/bmstu/DelayMapper.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class DelayMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException {
-        
+
         TextPair code;
         Text delayTime;
         context.write(code, delayTime);
