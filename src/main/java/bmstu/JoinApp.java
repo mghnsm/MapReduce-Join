37bb30db77945fde2c5f84e46f2b71be06963245
Anna <mghnsm@gmail.com>
2020-10-11 15:10:17 +0300

    Scripted auto-commit on change (2020-10-11 15:10:17) by gitwatch.sh

--- src/main/java/bmstu/JoinApp.java
+++ src/main/java/bmstu/JoinApp.java
@@ -3,7 +3,6 @@ package bmstu;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
