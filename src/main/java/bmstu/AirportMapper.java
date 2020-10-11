29881af562801da1b547703bbbe0485075641f0c
Anna <mghnsm@gmail.com>
2020-10-11 16:46:45 +0300

    Scripted auto-commit on change (2020-10-11 16:46:45) by gitwatch.sh

--- src/main/java/bmstu/AirportMapper.java
+++ src/main/java/bmstu/AirportMapper.java
@@ -12,7 +12,7 @@ public class AirportMapper extends Mapper<LongWritable, Text, TextPair, Text> {
         String line = value.toString();
         int separator = line.indexOf(",");
 
-        int code = Integer.parseInt(line.substring(0, separator));
+        int code = Integer.parseInt(line.substring(1, separator - 1));
         String name = line.substring(separator + 2, line.length() - 1);
 
         TextPair airportCode = new TextPair(Integer.toString(code), "0");
