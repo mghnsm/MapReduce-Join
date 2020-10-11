7d34c0c9a01143826a5a9a2c1974742470b325ab
Anna <mghnsm@gmail.com>
2020-10-11 13:29:27 +0300

    Scripted auto-commit on change (2020-10-11 13:29:27) by gitwatch.sh

--- src/main/java/bmstu/JoinReducer.java
+++ src/main/java/bmstu/JoinReducer.java
@@ -1,19 +1,17 @@
 package bmstu;
 
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
 
+import javax.naming.Context;
+import java.io.IOException;
 import java.util.Iterator;
 
 public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
     @Override
     protected void reduce(TextPair key, Iterable<Text> values, Context context) throws
             IOException, InterruptedException {
-        Iterator<Text> iter = values.iterator();
-        Text systemInfo = new Text(iter.next());
-        while (iter.hasNext()) {
-            Text call = iter.next();
-            Text outValue = new Text(call.toString() + "\t" + systemInfo.toString());
-            context.write(key.getFirst(), outValue);
+
         }
     }
 }
