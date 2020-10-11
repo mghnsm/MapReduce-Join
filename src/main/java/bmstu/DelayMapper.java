f27f03aeb45c79875b2a8f20aae8df2db33dfccc
Anna <mghnsm@gmail.com>
2020-10-11 12:52:23 +0300

    Scripted auto-commit on change (2020-10-11 12:52:23) by gitwatch.sh

--- src/main/java/bmstu/DelayMapper.java
+++ src/main/java/bmstu/DelayMapper.java
@@ -20,7 +20,7 @@ public class DelayMapper extends Mapper<LongWritable, Text, TextPair, Text> {
 
         if(delay != null && !delay.isEmpty()) {
             TextPair code = new TextPair(line[DEST_AIRPORT_ID], "0");
-            Text delayTime;
+            Text delayTime = line[ARR_DELAY_NEW];
             context.write(code, delayTime);
         }
     }
