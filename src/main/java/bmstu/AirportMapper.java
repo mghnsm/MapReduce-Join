0f5fce11a84db526a365d0756687a577e14ab5f9
Anna <mghnsm@gmail.com>
2020-10-14 13:26:53 +0300

    Scripted auto-commit on change (2020-10-14 13:26:53) by gitwatch.sh

--- src/main/java/bmstu/AirportMapper.java
+++ src/main/java/bmstu/AirportMapper.java
@@ -9,16 +9,18 @@ import java.io.IOException;
 public class AirportMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        if(key.get() == 0 || value.toString().isEmpty()) {
-            return;
+        if(key.get() != 0) {
+            if (value.toString().isEmpty()) {
+                return;
+            }
+
+            String line = value.toString();
+            int separator = line.indexOf(",");
+
+            int code = Integer.parseInt(line.substring(1, separator - 1));
+            String name = line.substring(separator + 2, line.length() - 1);
+
+            context.write(new TextPair(Integer.toString(code), "0"), new Text(name));
         }
-
-        String line = value.toString();
-        int separator = line.indexOf(",");
-
-        int code = Integer.parseInt(line.substring(1, separator - 1));
-        String name = line.substring(separator + 2, line.length() - 1);
-
-        context.write(new TextPair(Integer.toString(code), "0"), new Text(name));
     }
 }
